8d588a29bf323698215edb1e0887afa571d7f161
Konst_V <varjutin@yandex.ru>
2022-09-29 16:32:37 +0300

    init barnch

---
+++ game_2048/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Board from "../components/Board";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Board />
+    </div>
+  );
+};
+
+export default Home;
