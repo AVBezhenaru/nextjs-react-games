16c05ff10b0c8065e19f3d4fff1886c3accadab7
Lev <salamatinlefff@gmail.com>
2022-10-10 15:10:53 +0400

    fix: resetgame at set theme

--- hangman/src/components/App/App.tsx
+++ hangman/src/components/App/App.tsx
@@ -7,6 +7,7 @@ import { Layout } from '../Layout/Layout';
 const App: FC = () => (
   <Routes>
     <Route path="/" element={<Layout />}>
+      <Route index element={<ChangeTheme />} />
       <Route path="/theme" element={<ChangeTheme />} />
       <Route path="/game" element={<Game />} />
     </Route>
