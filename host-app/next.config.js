7ec0afdc42c34b7a3ceb3c75dbf854ceecfdc853
AVBezhenaru <bejena-ru@ya.ru>
2022-10-11 01:18:19 +0700

    domain

--- host-app/next.config.js
+++ host-app/next.config.js
@@ -8,7 +8,7 @@ module.exports = {
         new NextFederationPlugin({
           name: 'host',
           remotes: {
-            remote: process.env.DOMAIN,
+            remote: `remote@${process.env.DOMAIN}/remote.js`,
           },
         }),
       );
