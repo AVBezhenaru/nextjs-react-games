616fba9934fbd42d126523a1bddfee633b54e051
Lev <salamatinlefff@gmail.com>
2022-10-11 14:06:30 +0400

    feat: add home page

--- hangman/src/components/App/App.tsx
+++ hangman/src/components/App/App.tsx
@@ -1,13 +1,14 @@
 import React, { FC } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
-import { ChangeTheme, Game } from '../../pages';
+import { ChangeTheme, Game, Home } from '../../pages';
 import { Layout } from '../Layout/Layout';
 
 const App: FC = () => (
   <Routes>
     <Route path="/" element={<Layout />}>
-      <Route index element={<ChangeTheme />} />
+      <Route index element={<Navigate to="/home" replace />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/theme" element={<ChangeTheme />} />
       <Route path="/game" element={<Game />} />
     </Route>
--- hangman/src/pages/index.ts
+++ hangman/src/pages/index.ts
@@ -1,2 +1,3 @@
+export { Home } from './Home/Home';
 export { Game } from './Game/Game';
 export { ChangeTheme } from './ChangeTheme/ChangeTheme';
