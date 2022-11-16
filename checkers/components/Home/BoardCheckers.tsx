8fc93130597dc450b4250c4f4e6dcbf1e51d0135
Ilya <dinlion@yandex.ru>
2022-11-16 06:38:35 +0300

    lint

--- checkers/components/Home/BoardCheckers.tsx
+++ checkers/components/Home/BoardCheckers.tsx
@@ -224,7 +224,7 @@ const BoardCheckers: FC<BoardProps> = ({ board, setBoard, currentPlayer, current
 
       //? реализация передачи хода между игроками
       if (
-        ((board.lostBlackFigure?.length > prevBlack && banOnHitting && (((cell.y + 2) <= 7 ? cell.board.getCell(cell.x + 2, cell.y + 2)?.isEmpty() : false) && !cell.board.getCell(cell.x + 1, cell.y + 1)?.isEmpty() && cell.board.getCell(cell.x + 1, cell.y + 1)?.isFigure().color === 'black'))
+        ((board?.lostBlackFigure?.length > prevBlack && banOnHitting && (((cell.y + 2) <= 7 ? cell.board.getCell(cell.x + 2, cell.y + 2)?.isEmpty() : false) && !cell.board.getCell(cell.x + 1, cell.y + 1)?.isEmpty() && cell.board.getCell(cell.x + 1, cell.y + 1)?.isFigure().color === 'black'))
           || (board.lostWhiteFigure?.length > prevWhite && banOnHitting && (((cell.y + 2) <= 7 ? cell.board.getCell(cell.x + 2, cell.y + 2)?.isEmpty() : false) && !cell.board.getCell(cell.x + 1, cell.y + 1)?.isEmpty() && cell.board.getCell(cell.x + 1, cell.y + 1)?.isFigure().color === 'white')))
         ||
         ((board.lostBlackFigure?.length > prevBlack && banOnHitting && (((cell.y - 2) >= 0 ? cell.board.getCell(cell.x + 2, cell.y - 2)?.isEmpty() : false) && !cell.board.getCell(cell.x + 1, cell.y - 1)?.isEmpty() && cell.board.getCell(cell.x + 1, cell.y - 1)?.isFigure().color === 'black'))
