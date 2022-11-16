422d59ed13841f2940a2773da0b1bf870d60eded
Alina <alinka.9709@mail.ru>
2022-11-16 16:18:57 +0300

    local-fix

--- checkers/components/Modal/Modal.jsx
+++ checkers/components/Modal/Modal.jsx
@@ -1,37 +1,32 @@
-import React, { useEffect } from "react";
-import { CSSTransition } from "react-transition-group";
+import React, { useEffect } from 'react';
+import { CSSTransition } from 'react-transition-group';
 
-const Modal = props => {
+const Modal = (props) => {
   const nodeRef = React.useRef(null);
 
-  const closeOnEscapeKeyDown = e => {
+  const closeOnEscapeKeyDown = (e) => {
     if ((e.charCode || e.keyCode) === 27) {
       props.onClose();
     }
   };
 
   useEffect(() => {
-    document.body.addEventListener("keydown", closeOnEscapeKeyDown);
+    document.body.addEventListener('keydown', closeOnEscapeKeyDown);
     return function cleanup() {
-      document.body.removeEventListener("keydown", closeOnEscapeKeyDown);
+      document.body.removeEventListener('keydown', closeOnEscapeKeyDown);
     };
   }, []);
 
   return (
-    <CSSTransition
-      in={props.show}
-      unmountOnExit
-      timeout={{ enter: 0, exit: 300 }}
-      nodeRef={nodeRef}
-    >
+    <CSSTransition in={props.show} unmountOnExit timeout={{ enter: 0, exit: 300 }} nodeRef={nodeRef}>
       <div className="modal" onClick={props.onClose} ref={nodeRef}>
-        <div className="modal-content" onClick={e => e.stopPropagation()}>
+        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
           <div className="modal-header">
             <h4 className="modal-title">{props.title}</h4>
           </div>
           <div className="modal-body">{props.children}</div>
           <div className="modal-footer">
-            <button className="modal-button" onClick={props.onClose}>
+            <button className="modal-button" type="button" onClick={props.onClose}>
               Закрыть
             </button>
           </div>
