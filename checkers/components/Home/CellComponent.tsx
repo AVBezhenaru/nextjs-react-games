c15bcc513fc299d4a9a17c56c085ff261102f4dd
Ilya <dinlion@yandex.ru>
2022-11-16 04:13:49 +0300

    FC

--- checkers/components/Home/CellComponent.tsx
+++ checkers/components/Home/CellComponent.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { useState } from "react";
 import { Cell } from "../../model/Cell";
 import Image from 'next/image';
 
@@ -8,7 +8,7 @@ interface CellProps {
   click: (cell: Cell) => void;
 }
 
-const CellComponent: FC<CellProps> = ({cell, selected, click}) => {
+const CellComponent = ({cell, selected, click}) => {
   const [trueColor, setTrueColor] = useState(false);
 
   return ( 
