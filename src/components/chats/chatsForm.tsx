8f2cc50296d3084294512b115d8b4f81103a2028
BabyBaliis <sovvysha010@yandex.ru>
2022-11-24 16:25:35 +0300

    Добавлена верска, вход и выход, локальное хранение пользователя

--- src/components/chats/chatsForm.tsx
+++ src/components/chats/chatsForm.tsx
@@ -3,7 +3,7 @@ import {withRouter} from "next/router";
 import Header from "../header/header";
 import SideBar from "../header/sideBar";
 import {DivPage, Section} from "../profile/profileStyle";
-import {Chat, Page, Friend, SelectFriend, FullChat} from "./chatsFormStyle";
+import {Chat, Page, Friend, SelectFriend, FullChat, SentMessage, InputMessage} from "./chatsFormStyle";
 import {Search} from "../header/headerStyle";
 
 const ChatsForm = () => {
@@ -14,15 +14,24 @@ const ChatsForm = () => {
                 <SideBar/>
                 <Page>
                     <Friend>
-                        <Search type={'text'} placeholder={'Search'}/>
+                        <span style={{display:'flex', flexDirection:'column', gap:'20px', padding:' 0 0 10px 0' , borderBottom:'1px solid rgba(128, 128, 128, 0.11)'}}>
+                            <img src={'https://consultus.org/cus/wp-content/uploads/2015/06/Avatare-w-2.jpg'}
+                                 alt={''}
+                                 style={{width: '40px', height: 'auto'}}/>
+                        <Search type={'text'} placeholder={'Search'} style={{width:'300px'}}/>
+                        </span>
+
                     </Friend>
                     <Chat>
                         <SelectFriend>
-                            nknk
+                            Тут будет выбранный друг
                         </SelectFriend>
                         <FullChat>
-                            hkkkh
+                            переписка
                         </FullChat>
+                        <SentMessage>
+                            <InputMessage placeholder={'Type a message'}/>
+                        </SentMessage>
                     </Chat>
                 </Page>
             </DivPage>
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,16 +1,32 @@
-import React, {ReactNode} from "react";
+import React, {ReactNode, useEffect, useState} from "react";
 import {SectionLayout} from "./layoutStyle";
 
 type LayoutProps = {
-  children: ReactNode;
+    children: ReactNode;
 };
 
 const Layout = ({children}: LayoutProps) => {
-  return(
-      <SectionLayout>
-        {children}
-      </SectionLayout>
-  )
-};
+    const [showing, setShowing] = useState(false);
+
+    useEffect(() => {
+        setShowing(true);
+    }, []);
+
+    if (!showing) {
+        return null;
+    }
+
+    if (typeof window === 'undefined') {
+        return <></>;
+    } else {
+        return (
+            <SectionLayout>
+                {children}
+            </SectionLayout>
+        )
+    }
+    ;
+}
+
 
 export {Layout}
