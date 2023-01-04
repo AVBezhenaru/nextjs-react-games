01688c486e854f91f4a69c45e81455965c5161de
AngelinaSun <angelina_gaydukova@mail.ru>
2023-01-04 21:17:34 +0300

    the game is working

--- solitaire/components/Game/Stack/Stack.tsx
+++
@@ -1,3 +0,0 @@
-import { Stack } from './StackStyle';
-
-export const StackCards = ({ id, isReady }) => <Stack id={id}>{isReady && <div />} </Stack>;
--- solitaire/components/Game/Stack/StackStyle.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Stack = styled.div`
-  position: relative;
-  height: 138px;
-  width: 100px;
-  background-color: rgba(0, 0, 0, 0.4);
-  box-shadow: -3px -3px 10px #000 inset;
-`;
