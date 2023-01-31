67266dd29241c4753eb0974301ae60214e0444cc
yushever <iushina.tatiana@gmail.com>
2023-01-31 23:39:04 +0500

    main page game2048 design

--- game2048/components/MainPage/MainPage.tsx
+++ game2048/components/MainPage/MainPage.tsx
@@ -1,4 +1,6 @@
 import styles from '../MainPage/MainPage.module.scss';
+import classes from '../MainPage/ButtonsStyle.module.scss';
+
 import React from 'react';
 import Link from 'next/link';
 
@@ -6,7 +8,9 @@ const MainPage = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.info}>
-        <h2 className={styles.heading}>2048</h2>
+        <div className={styles.container}>
+          <div className={styles.heading}>2048</div>
+        </div>
         <h3 className={styles.header}>How to play?</h3>
         <div className={styles.instructions}>
           Use your arrow keys to move the tiles. Tiles with the same number merge into one when they
@@ -14,14 +18,28 @@ const MainPage = () => {
         </div>
       </div>
       <div className={styles.buttons}>
-        <button className={styles.button}>
-          <Link href="./game2048/game">PLAY</Link>
+        <button className={classes.btn}>
+          <span className={classes.shadow}></span>
+          <span className={classes.depth}></span>
+          <span className={classes.content}>
+            <Link href="./game2048/game">PLAY</Link>
+          </span>
         </button>
-        <button className={styles.button}>
-          <Link href="./game2048/settings">SETTINGS</Link>
+
+        <button className={classes.btn}>
+          <span className={classes.shadow}></span>
+          <span className={classes.depth}></span>
+          <span className={classes.content}>
+            <Link href="./game2048/settings">SETTINGS</Link>
+          </span>
         </button>
-        <button className={styles.button}>
-          <Link href="./game2048/score-table">SCORES</Link>
+
+        <button className={classes.btn}>
+          <span className={classes.shadow}></span>
+          <span className={classes.depth}></span>
+          <span className={classes.content}>
+            <Link href="./game2048/score-table">SCORES</Link>{' '}
+          </span>
         </button>
       </div>
     </div>
