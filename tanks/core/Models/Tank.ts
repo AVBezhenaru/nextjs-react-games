4520e582fcfc65c09611c544e7f70f1d5c148556
simskiy <simskiy@gmail.com>
2023-02-17 17:47:53 +0300

    Прототип работы коллизии (чтобы через стенки не проходил)

--- tanks/core/Models/Tank.ts
+++ tanks/core/Models/Tank.ts
@@ -1,7 +1,15 @@
 /* eslint-disable lines-between-class-members */
 import { playerPrimary } from '../tileMap';
 import { switchFrame } from '../utils/switchFrame';
-import { FIELD_SIZE, PLAYER_DEFAULT_SPAWN_POSITIONS } from '../../config';
+import {
+  FIELD_SIZE,
+  TILE_SIZE,
+  FIELD_TILE_COUNT,
+  PLAYER_DEFAULT_SPAWN_POSITIONS,
+} from '../../config';
+import { fieldHeight } from '../../../tetris/models/static-data';
+
+// import { Land } from './Land';
 
 export enum ControlKey {
   up = 'ArrowUp',
@@ -34,6 +42,7 @@ interface ITank {
 }
 
 export class Tank implements ITank {
+  land: number[][];
   x: number;
   y: number;
   view: number[];
@@ -45,7 +54,7 @@ export class Tank implements ITank {
   tank_width: number;
   tank_height: number;
 
-  constructor() {
+  constructor(land: number[][]) {
     this.x = PLAYER_DEFAULT_SPAWN_POSITIONS[0].x;
     this.y = PLAYER_DEFAULT_SPAWN_POSITIONS[0].y;
     [this.view] = [...playerPrimary.rank_1.up];
@@ -55,31 +64,84 @@ export class Tank implements ITank {
     this.type = Type.PLAYER1;
     this.frames = playerPrimary.rank_1.up;
     [this.tank_width, this.tank_height] = [this.view[2], this.view[3]];
+    this.land = land;
+  }
+
+  isCanMove(direction: ControlKey) {
+    const firstCol = Math.round(this.x / TILE_SIZE);
+    const firstRow = Math.round(this.y / TILE_SIZE);
+    const secondCol = Math.round((this.x + this.tank_width) / TILE_SIZE);
+    const secondRow = Math.round((this.y + this.tank_height) / TILE_SIZE);
+
+    const [stopFirstRow, stopFirstCol] = this.findBarrier(firstRow, firstCol);
+    // const [stopSecondRow, stopSecondCol] = this.findBarrier(secondRow, secondCol);
+    // console.log(this.y, stopSecondRow * TILE_SIZE + TILE_SIZE);
+    switch (true) {
+      case direction === ControlKey.up:
+        return (
+          this.y >= 0 &&
+          this.y > stopFirstRow * TILE_SIZE + TILE_SIZE &&
+          this.y + this.tank_width > stopFirstRow * TILE_SIZE + TILE_SIZE
+        );
+      case direction === ControlKey.right:
+        return this.x <= FIELD_SIZE - this.tank_width;
+      case direction === ControlKey.down:
+        return this.y <= FIELD_SIZE - this.tank_height;
+      case direction === ControlKey.left:
+        return this.x >= 0;
+    }
+  }
+
+  findBarrier(row: number, col: number) {
+    while (row > 0 && col > 0) {
+      console.log(row, col);
+      if (this.land[row][col]) {
+        return [row, col];
+      }
+      row -= 1;
+      console.log(row, col);
+    }
+    return [row, col];
   }
 
   moveTank(key: Set<unknown>) {
     switch (true) {
       case key.has(ControlKey.up):
-        if (this.y > 0) {
-          this.y -= this.speed;
-          this.view = switchFrame(playerPrimary[this.rank].up, this.view);
+        if (this.y - this.speed > 0) {
+          this.y -= this.isCanMove(ControlKey.up) ? this.speed : 0;
+        } else {
+          this.y = 0;
         }
+        this.view = switchFrame(playerPrimary[this.rank].up, this.view);
+
         break;
       case key.has(ControlKey.right):
-        if (this.x < FIELD_SIZE - this.tank_width) {
-          this.x += this.speed;
+        if (this.isCanMove(ControlKey.right)) {
+          if (this.x + this.speed + this.tank_width < FIELD_SIZE) {
+            this.x += this.speed;
+          } else {
+            this.x = FIELD_SIZE - this.tank_width;
+          }
           this.view = switchFrame(playerPrimary[this.rank].right, this.view);
         }
         break;
       case key.has(ControlKey.down):
-        if (this.y < FIELD_SIZE - this.tank_height) {
-          this.y += this.speed;
+        if (this.isCanMove(ControlKey.down)) {
+          if (this.y + this.speed + this.tank_height < FIELD_SIZE) {
+            this.y += this.speed;
+          } else {
+            this.y = FIELD_SIZE - this.tank_height;
+          }
           this.view = switchFrame(playerPrimary[this.rank].down, this.view);
         }
         break;
       case key.has(ControlKey.left):
-        if (this.x > 0) {
-          this.x -= this.speed;
+        if (this.isCanMove(ControlKey.left)) {
+          if (this.x - this.speed > 0) {
+            this.x -= this.speed;
+          } else {
+            this.x = 0;
+          }
           this.view = switchFrame(playerPrimary[this.rank].left, this.view);
         }
         break;
--- tanks/core/World/World.ts
+++ tanks/core/World/World.ts
@@ -9,13 +9,13 @@ import { Land } from '../Models/Land';
 // }
 
 class World {
-  playerTank_1 = new Tank();
-
-  playerTank_2: any = null;
-
   currentLevel = 1;
 
   land: any = new Land(this.currentLevel);
+
+  playerTank_1 = new Tank(this.land.curLevel);
+
+  playerTank_2: any = null;
 
   enemyTanks: any = [];
 }
