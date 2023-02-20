bbacb63db7190b852e91f9f16f4fcf4be0eeb9b3
simskiy <simskiy@gmail.com>
2023-02-20 19:32:50 +0300

    сетка для отладки

--- tanks/index.tsx
+++ tanks/index.tsx
@@ -2,10 +2,12 @@ import { FC, ReactElement } from 'react';
 
 import styles from './index.module.scss';
 import Canvas from './core/Canvas/Canvas';
+import { Grid } from './grid/Grid';
 
 const Game: FC = (): ReactElement => (
   <div className={styles.game}>
     <Canvas />
+    <Grid />
   </div>
 );
 
