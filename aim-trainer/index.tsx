e370a60da7c4af7cd8bd2899764b9b6e9eb67446
vlad-tokarev799 <vlad.tokarev.200247@gmail.com>
2023-04-27 15:21:17 +0800

    Добавил возможность поменять уровень сложности после окончания игры

--- aim-trainer/index.tsx
+++ aim-trainer/index.tsx
@@ -1,13 +1,20 @@
 import { ThemeProvider } from 'styled-components';
+import { useEffect } from 'react';
 
 import { Lobby } from './components/lobby/lobby';
 import { AimTrainerTheme } from './themes/default-theme';
 import { StyledAimTrainer } from './index.styles';
 
-export const AimTrainerGame = () => (
-  <ThemeProvider theme={AimTrainerTheme}>
-    <StyledAimTrainer>
-      <Lobby />
-    </StyledAimTrainer>
-  </ThemeProvider>
-);
+export const AimTrainerGame = () => {
+  useEffect(() => {
+    document.title = 'Aim Trainer Game';
+  }, []);
+
+  return (
+    <ThemeProvider theme={AimTrainerTheme}>
+      <StyledAimTrainer>
+        <Lobby />
+      </StyledAimTrainer>
+    </ThemeProvider>
+  );
+};
