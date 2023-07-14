66f4ca0fcdb18f8b9566e0df0003b446e2fb33d0
GlebTroyakov <glebtroyakov@gmail.com>
2023-07-14 11:19:42 +0300

    create currency folder with CurrencyPanel and components: UserTable and GraphCurrency, added filed currency in admin-navigate in arr.tsx

---
+++ currency/CurrencyPanel.tsx
@@ -0,0 +1,14 @@
+import { Page, Panel } from './CurrencyPanelStyle';
+
+export const CurrencyPanel = () => (
+  <Page>
+    <Panel>
+      <div>UsersTable</div>
+      <div>GraphCurrency</div>
+    </Panel>
+  </Page>
+
+  // <Provider store={store}>
+
+  // </Provider>
+);
---
+++ currency/CurrencyPanelStyle.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Page = styled.div`
+  display: flex;
+  margin: 0;
+  padding: 0;
+  background-color: #e8e8e8;
+  min-height: 100vh;
+  width: 100%;
+`;
+
+export const Panel = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 15px;
+  padding: 25px;
+  margin: 25px auto;
+  background-color: tomato;
+  border-radius: 15px;
+`;
