8447bc5b8dd0745d1f455f6e7db0f74070d3945d
encore <encore>
2023-07-19 11:19:42 +0300

    fix first bug

--- mario-bros/components/Camera.ts
+++ mario-bros/components/Camera.ts
@@ -1,6 +1,10 @@
 import Vec2 from './Vec2';
 
 export default class Camera {
+  private pos: Vec2;
+
+  private size: Vec2;
+
   constructor() {
     this.pos = new Vec2(0, 0);
     this.size = new Vec2(800, 650);
